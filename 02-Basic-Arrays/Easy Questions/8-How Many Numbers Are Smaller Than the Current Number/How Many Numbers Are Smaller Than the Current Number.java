f811c8e6f4483e2808aeb1dfe951069e712312c1
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-12-25 21:42:33 +0530

    Create How Many Numbers Are Smaller Than the Current Number.java

---
+++ 02-Basic-Arrays/Easy Questions/8-How Many Numbers Are Smaller Than the Current Number/How Many Numbers Are Smaller Than the Current Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        
+        int [] smaller = new int[nums.length];
+        for(int i=0 ; i<nums.length ; i++){
+            for(int j=0; j<nums.length ; j++){
+                if(i == j){
+                    continue;
+                }
+                if(nums[j] < nums[i]){
+                    smaller[i]++;
+                }
+            }
+        }
+
+        return smaller;
+    }
+}
